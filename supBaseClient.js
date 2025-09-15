ce6254035f0b7a8a9090097bb21996f8cf1a70c2
kanha077 <yashvijayvergiya911@gmail.com>
2025-09-16 00:19:05 +0530

    kjdh

--- supBaseClient.js
+++ supBaseClient.js
@@ -1,9 +1,14 @@
-// js/supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// Replace with your Supabase project credentials
-const SUPABASE_URL = "https://wraemxcqjycuddhfgjtt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyYWVteGNxanljdWRkaGZnanR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDc2MTYsImV4cCI6MjA3MjkyMzYxNn0.QyBb8idQcPmWJUQ7sMorfmnGp7fZU6DpuA1U79KpgCQ";
+const SUPABASE_URL = "YOUR_SUPABASE_URL"; // Make sure your URL is here
+const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY"; // Make sure your Key is here
 
-// Export client
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// UPDATED: Added options object to use Local Storage
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: localStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  },
+});
