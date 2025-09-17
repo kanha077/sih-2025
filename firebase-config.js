c54eff1bb10cf03d258ece16199bc58aea171e42
kanha077 <yashvijayvergiya911@gmail.com>
2025-09-17 23:59:37 +0530

    kjdh

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
 
 
+
 // firebase-config.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-          const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+
+        const firebaseConfig = {
   apiKey: "AIzaSyBr347m53UAvyMcTOjQwrIN_kOFYcenIMM",
   authDomain: "ananda-74477.firebaseapp.com",
   databaseURL: "https://ananda-74477-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -15,11 +17,10 @@ import { getStorage } from "firebase/storage";
   appId: "1:525918416681:web:fb89b43c7a395816021138",
   measurementId: "G-CL5KLMJD6N"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ Export properly
+// ✅ Export
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
